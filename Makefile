055be02c2edb8eb040f25aa8facd9377e188d356
Matthew Brisebois <matthew@webheroes.ca>
2022-04-19 11:44:19 -0600

    Change dist file names and add dev builds

--- Makefile
+++ Makefile
@@ -49,6 +49,7 @@ clean-files-all-force:	clean-remove-chaff
 # NPM packaging
 #
 prepare-package:
+	FILENAME=holo-hash.js WEBPACK_MODE=development npm run build
 	npm run build
 	gzip -kf dist/*.js
 preview-package:	clean-files test prepare-package
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const webpack			= require('webpack');
 const TerserPlugin		= require("terser-webpack-plugin");
 
+const WEBPACK_MODE		= process.env.WEBPACK_MODE || "production";
+const FILENAME			= process.env.FILENAME || "holo-hash.prod.js";
+
 module.exports = {
     target: 'node',
-    mode: 'production', // production | development
+    mode: WEBPACK_MODE,
     entry: [ './src/index.js' ],
     output: {
-	filename: 'holo-hash.bundled.js',
-	globalObject: 'this',
+	filename:	FILENAME,
+	globalObject:	"this",
 	library: {
-	    "name": "holohash",
-	    "type": "umd",
+	    "name":	"holohash",
+	    "type":	"umd",
 	},
     },
     stats: {
